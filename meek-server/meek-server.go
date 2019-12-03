c76538ff49396f543ac01c33a93b0921aee12164
David Fifield <david@bamsoftware.com>
2019-12-02 19:24:04 -0700

    turbotunnel in meek-server.

--- meek-server/meek-server.go
+++ meek-server/meek-server.go
@@ -20,6 +20,7 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"flag"
 	"fmt"
@@ -38,7 +39,9 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/lucas-clemente/quic-go"
 	"git.torproject.org/pluggable-transports/goptlib.git"
+	"git.torproject.org/pluggable-transports/meek.git/common/encapsulation"
 	"golang.org/x/crypto/acme/autocert"
 	"golang.org/x/net/http2"
 )
@@ -47,25 +50,24 @@ const (
 	programVersion = "0.34"
 
 	ptMethodName = "meek"
-	// Reject session ids shorter than this, as a weak defense against
-	// client bugs that send an empty session id or something similarly
-	// likely to collide.
-	minSessionIDLength = 8
 	// The largest request body we are willing to process, and the largest
 	// chunk of data we'll send back in a response.
 	maxPayloadLength = 0x10000
-	// How long we try to read something back from the OR port before
-	// returning the response.
+	// How long we try to read from the OR port before closing a response.
 	turnaroundTimeout = 10 * time.Millisecond
 	// Passed as ReadTimeout and WriteTimeout when constructing the
 	// http.Server.
 	readWriteTimeout = 20 * time.Second
-	// Cull unused session ids (with their corresponding OR port connection)
-	// if we haven't seen any activity for this long.
-	maxSessionStaleness = 120 * time.Second
 	// How long to wait for ListenAndServe or ListenAndServeTLS to return an
 	// error before deciding that it's not going to return.
 	listenAndServeErrorTimeout = 100 * time.Millisecond
+
+	// The ALPN field value for the tunnelled QUIC–TLS connection.
+	quicNextProto = "meek-quic"
+	// How long to wait for a handshake to complete at the inner QUIC layer.
+	quicHandshakeTimeout = 30 * time.Second
+	// How long before timing out connections at the inner QUIC layer.
+	quicIdleTimeout = 30 * time.Minute
 )
 
 var ptInfo pt.ServerInfo
@@ -78,36 +80,40 @@ func httpInternalServerError(w http.ResponseWriter) {
 	http.Error(w, "Internal server error.", http.StatusInternalServerError)
 }
 
-// Every session id maps to an existing OR port connection, which we keep open
-// between received requests. The first time we see a new session id, we create
-// a new OR port connection.
-type Session struct {
-	Or       *net.TCPConn
-	LastSeen time.Time
-}
-
-// Mark a session as having been seen just now.
-func (session *Session) Touch() {
-	session.LastSeen = time.Now()
-}
-
-// Is this session old enough to be culled?
-func (session *Session) IsExpired() bool {
-	return time.Since(session.LastSeen) > maxSessionStaleness
-}
-
 // There is one state per HTTP listener. In the usual case there is just one
 // listener, so there is just one global state. State also serves as the http
 // Handler.
 type State struct {
-	sessionMap map[string]*Session
-	lock       sync.Mutex
+	conn *QueuePacketConn
 }
 
-func NewState() *State {
-	state := new(State)
-	state.sessionMap = make(map[string]*Session)
-	return state
+func NewState(localAddr net.Addr, cert *tls.Certificate) (*State, error) {
+	pconn := NewQueuePacketConn(localAddr, quicIdleTimeout)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{*cert},
+		NextProtos:   []string{quicNextProto},
+	}
+	quicConfig := &quic.Config{
+		HandshakeTimeout: quicHandshakeTimeout,
+		IdleTimeout:      quicIdleTimeout,
+	}
+	ln, err := quic.Listen(pconn, tlsConfig, quicConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		defer ln.Close()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		err := acceptSessions(ctx, ln)
+		if err != nil {
+			log.Printf("acceptSessions: %v", err)
+		}
+	}()
+
+	return &State{pconn}, nil
 }
 
 func (state *State) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -132,40 +138,6 @@ func (state *State) Get(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("I’m just a happy little web server.\n"))
 }
 
-// Get a string representing the original client address, if available, as a
-// "host:port" string suitable to pass as the addr parameter to pt.DialOr. Never
-// fails: if the original client address is not available, returns "". If the
-// original client address is available, the returned port number is always 1.
-func getUseraddr(req *http.Request) string {
-	ip, err := originalClientIP(req)
-	if err != nil {
-		return ""
-	}
-	return net.JoinHostPort(ip.String(), "1")
-}
-
-// Look up a session by id, or create a new one (with its OR port connection) if
-// it doesn't already exist.
-func (state *State) GetSession(sessionID string, req *http.Request) (*Session, error) {
-	state.lock.Lock()
-	defer state.lock.Unlock()
-
-	session := state.sessionMap[sessionID]
-	if session == nil {
-		// log.Printf("unknown session id %q; creating new session", sessionID)
-
-		or, err := pt.DialOr(&ptInfo, getUseraddr(req), ptMethodName)
-		if err != nil {
-			return nil, err
-		}
-		session = &Session{Or: or}
-		state.sessionMap[sessionID] = session
-	}
-	session.Touch()
-
-	return session, nil
-}
-
 // scrubbedAddr is a phony net.Addr that returns "[scrubbed]" for all calls.
 type scrubbedAddr struct{}
 
@@ -184,86 +156,136 @@ func scrubError(err error) error {
 	return err
 }
 
-// Feed the body of req into the OR port, and write any data read from the OR
-// port back to w.
-func transact(session *Session, w http.ResponseWriter, req *http.Request) error {
-	body := http.MaxBytesReader(w, req.Body, maxPayloadLength+1)
-	_, err := io.Copy(session.Or, body)
-	if err != nil {
-		return fmt.Errorf("error copying body to ORPort: %s", scrubError(err))
-	}
-
-	buf := make([]byte, maxPayloadLength)
-	session.Or.SetReadDeadline(time.Now().Add(turnaroundTimeout))
-	n, err := session.Or.Read(buf)
-	if err != nil {
-		if e, ok := err.(net.Error); !ok || !e.Timeout() {
-			httpInternalServerError(w)
-			// Don't scrub err here because it always refers to localhost.
-			return fmt.Errorf("reading from ORPort: %s", err)
-		}
-	}
-	// log.Printf("read %d bytes from ORPort: %q", n, buf[:n])
-	// Set a Content-Type to prevent Go and the CDN from trying to guess.
-	w.Header().Set("Content-Type", "application/octet-stream")
-	n, err = w.Write(buf[:n])
-	if err != nil {
-		return fmt.Errorf("error writing to response: %s", scrubError(err))
-	}
-	// log.Printf("wrote %d bytes to response", n)
-	return nil
-}
-
-// Handle a POST request. Look up the session id and then do a transaction.
+// Handle a POST request.
 func (state *State) Post(w http.ResponseWriter, req *http.Request) {
-	sessionID := req.Header.Get("X-Session-Id")
-	if len(sessionID) < minSessionIDLength {
-		httpBadRequest(w)
-		return
-	}
+	defer req.Body.Close()
+	body := http.MaxBytesReader(w, req.Body, maxPayloadLength+1)
 
-	session, err := state.GetSession(sessionID, req)
+	var clientID ClientID
+	_, err := io.ReadFull(body, clientID[:])
 	if err != nil {
-		log.Print(err)
-		httpInternalServerError(w)
+		// The request body didn't even contain enough bytes for the
+		// ClientID. This could be because the client is shaping its
+		// request sizes, or we're being talked to by a non-meek client,
+		// or a receive error occurred. In any case, we cannot proceed
+		// without a ClientID. We could do something like send padding
+		// here.
 		return
 	}
 
-	err = transact(session, w, req)
-	if err != nil {
-		log.Print(err)
-		state.CloseSession(sessionID)
-		return
-	}
-}
-
-// Remove a session from the map and closes its corresponding OR port
-// connection. Does nothing if the session id is not known.
-func (state *State) CloseSession(sessionID string) {
-	state.lock.Lock()
-	defer state.lock.Unlock()
-	// log.Printf("closing session %q", sessionID)
-	session, ok := state.sessionMap[sessionID]
-	if ok {
-		session.Or.Close()
-		delete(state.sessionMap, sessionID)
-	}
-}
-
-// Loop forever, checking for expired sessions and removing them.
-func (state *State) ExpireSessions() {
+	// Read incoming packets.
 	for {
-		time.Sleep(maxSessionStaleness / 2)
-		state.lock.Lock()
-		for sessionID, session := range state.sessionMap {
-			if session.IsExpired() {
-				// log.Printf("deleting expired session %q", sessionID)
-				session.Or.Close()
-				delete(state.sessionMap, sessionID)
+		p, err := encapsulation.ReadData(body)
+		if err != nil {
+			break
+		}
+		state.conn.QueueIncoming(p, clientID)
+	}
+
+	// Write outgoing packets, if any, up to turnaroundTimeout.
+	w.Header().Set("Content-Type", "application/octet-stream")
+	outgoing := state.conn.OutgoingQueue(clientID)
+	timer := time.NewTimer(turnaroundTimeout)
+loop:
+	for {
+		select {
+		case <-timer.C:
+			break loop
+		case p := <-outgoing:
+			_, err := encapsulation.WriteData(w, p)
+			if err != nil {
+				break loop
+			}
+			// Flush after each chunk, this is important for
+			// latency.
+			if w, ok := w.(http.Flusher); ok {
+				w.Flush()
 			}
 		}
-		state.lock.Unlock()
 	}
+}
+
+func acceptSessions(ctx context.Context, ln quic.Listener) error {
+	for {
+		sess, err := ln.Accept(ctx)
+		if err != nil {
+			if err, ok := err.(*net.OpError); ok && err.Temporary() {
+				continue
+			}
+			return err
+		}
+
+		go func() {
+			defer sess.Close()
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
+
+			err := acceptStreams(ctx, sess)
+			if err != nil {
+				log.Printf("error in acceptStreams: %v", err)
+			}
+		}()
+	}
+}
+
+func acceptStreams(ctx context.Context, sess quic.Session) error {
+	for {
+		stream, err := sess.AcceptStream(ctx)
+		if err != nil {
+			if err, ok := err.(*net.OpError); ok && err.Temporary() {
+				continue
+			}
+			return err
+		}
+
+		go func() {
+			defer stream.Close()
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
+
+			err := handleStream(ctx, stream)
+			if err != nil {
+				log.Printf("error in handleStream: %v", err)
+			}
+		}()
+	}
+}
+
+func handleStream(ctx context.Context, stream quic.Stream) error {
+	// TODO: USERADDR
+	or, err := pt.DialOr(&ptInfo, "", ptMethodName)
+	if err != nil {
+		return err
+	}
+	defer or.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		io.Copy(or, stream)
+		or.Close()
+	}()
+	go func() {
+		defer wg.Done()
+		io.Copy(stream, or)
+		stream.Close()
+		or.Close()
+	}()
+
+	wgChan := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(wgChan)
+	}()
+	select {
+	case <-wgChan:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
+	return nil
 }
 
 func initServer(addr *net.TCPAddr,
@@ -279,9 +301,14 @@ func initServer(addr *net.TCPAddr,
 		return nil, fmt.Errorf("cannot listen on port %d; configure a port using ServerTransportListenAddr", addr.Port)
 	}
 
-	state := NewState()
-	go state.ExpireSessions()
-
+	cert, err := generateTLSCertificate()
+	if err != nil {
+		return nil, err
+	}
+	state, err := NewState(addr, cert)
+	if err != nil {
+		return nil, err
+	}
 	server := &http.Server{
 		Addr:         addr.String(),
 		Handler:      state,
@@ -296,7 +323,7 @@ func initServer(addr *net.TCPAddr,
 	// server.TLSConfig properly. An alternative would be to make a dummy
 	// net.Listener, call Serve on it, and let it return.
 	// https://github.com/golang/go/issues/16588#issuecomment-237386446
-	err := http2.ConfigureServer(server, nil)
+	err = http2.ConfigureServer(server, nil)
 	if err != nil {
 		return server, err
 	}
