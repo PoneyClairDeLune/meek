ae328538b8b582f2e594d8e3fcf192e8f70b8b16
David Fifield <david@bamsoftware.com>
2021-09-02 07:06:23 -0600

    Remove TestCopyPublicFieldsHTTPTransport.
    This ought to have been removed in
    bc887de694bc2d2381af099d5c38f0e9efd76c4b.
    
    https://bugs.torproject.org/tpo/anti-censorship/pluggable-transports/meek/40002

--- meek-client/utls_test.go
+++ meek-client/utls_test.go
@@ -11,41 +11,6 @@ import (
 	utls "github.com/refraction-networking/utls"
 )
 
-func TestCopyPublicFieldsHTTPTransport(t *testing.T) {
-	src := http.DefaultTransport.(*http.Transport)
-	dst := &http.Transport{}
-	copyPublicFields(dst, src)
-	// Test various fields that we might care about a copy of http.Transport
-	// having.
-	if dst.DisableKeepAlives != src.DisableKeepAlives {
-		t.Errorf("mismatch on DisableKeepAlives")
-	}
-	if dst.DisableCompression != src.DisableCompression {
-		t.Errorf("mismatch on DisableCompression")
-	}
-	if dst.MaxIdleConns != src.MaxIdleConns {
-		t.Errorf("mismatch on MaxIdleConns")
-	}
-	if dst.MaxIdleConnsPerHost != src.MaxIdleConnsPerHost {
-		t.Errorf("mismatch on MaxIdleConnsPerHost")
-	}
-	if dst.MaxConnsPerHost != src.MaxConnsPerHost {
-		t.Errorf("mismatch on MaxConnsPerHost")
-	}
-	if dst.IdleConnTimeout != src.IdleConnTimeout {
-		t.Errorf("mismatch on IdleConnTimeout")
-	}
-	if dst.ResponseHeaderTimeout != src.ResponseHeaderTimeout {
-		t.Errorf("mismatch on ResponseHeaderTimeout")
-	}
-	if dst.ExpectContinueTimeout != src.ExpectContinueTimeout {
-		t.Errorf("mismatch on ExpectContinueTimeout")
-	}
-	if dst.MaxResponseHeaderBytes != src.MaxResponseHeaderBytes {
-		t.Errorf("mismatch on MaxResponseHeaderBytes")
-	}
-}
-
 // Test that the name lookup of NewUTLSRoundTripper is case-insensitive.
 func TestNewUTLSRoundTripperCase(t *testing.T) {
 	mixed, err := NewUTLSRoundTripper("HelloFirefox_Auto", nil, nil)
